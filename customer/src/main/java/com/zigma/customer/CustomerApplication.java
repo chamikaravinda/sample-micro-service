d1af0c2e179a06bcae5b960cc8563835484f2b7e
Chamika Ravinda <chamikaravinda@gmail.com>
2022-03-02 22:16:43 +0530

    registering the clients with eureka server

--- customer/src/main/java/com/zigma/customer/CustomerApplication.java
+++ customer/src/main/java/com/zigma/customer/CustomerApplication.java
@@ -2,12 +2,10 @@ package com.zigma.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(
-        scanBasePackages = {
-                "com.zigma.customer",
-        }
-)
+@SpringBootApplication
+@EnableEurekaClient
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class,args);
